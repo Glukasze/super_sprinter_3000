6caed194205547e2ad5906439f124de2e875b3cb
Gracjan <gracjan.luk@gmail.com>
2020-09-13 21:40:37 +0200

    implemented main page and add-story page

--- data_handler.py
+++ data_handler.py
@@ -1,10 +1,45 @@
 import csv
 import os
 
-DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'data.csv'
+DATA_FILE_PATH = os.getenv(
+    'DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'H:\Coding\CodeCool\Workbench\super_sprinter_3000\data.csv'
 DATA_HEADER = ['id', 'title', 'user_story', 'acceptance_criteria', 'business_value', 'estimation', 'status']
 STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
 
+user_stories = [{'id': "0", 'title': "test", 'user_story': "I'm writing to check if everything works",
+                 'acceptance_criteria': "must work without errors", 'business_value': "1500", 'estimation': "8",
+                 'status': "in progress"},
+                {'id': "1", 'title': "test_1", 'user_story': "I'm writing to check if everything works",
+                 'acceptance_criteria': "must work without errors", 'business_value': "1500", 'estimation': "8",
+                 'status': "in progress"}]
+
+
+def get_id():
+    temp = 0
+    for story in user_stories:
+        if int(story["id"]) > temp:
+            temp = int(story["id"])
+    return str(temp + 1)
+
+
+def add_story(story):
+    story["id"] = get_id()
+    user_stories.append(story)
+
 
 def get_all_user_story():
-    return []
+    result = []
+    with open(DATA_FILE_PATH) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for line in csv_reader:
+            result.append(line)
+
+    return result
+
+
+def save_story(story):
+    result = get_all_user_story()
+    result.append(story)
+    with open(DATA_FILE_PATH, "w") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(result)
--- server.py
+++ server.py
@@ -5,12 +5,26 @@ import data_handler
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/list')
-def route_list():
-    user_stories = data_handler.get_all_user_story()
+@app.route('/', methods=["GET", "POST"])
+def route_index():
+    user_stories = data_handler.user_stories
+    headers = data_handler.DATA_HEADER
 
-    return render_template('list.html', user_stories=user_stories)
+    return render_template('index.html', user_stories=user_stories, headers=headers)
+
+
+@app.route("/story", methods=["GET", "POST"])
+def route_story():
+    if request.method == "POST":
+        data_handler.add_story(dict(request.form))
+        return redirect(url_for("route_index"))
+    return render_template("story.html")
+
+
+@app.route("/post", methods=["POST"])
+def route_post():
+    pass
+
 
 if __name__ == '__main__':
     app.run(
